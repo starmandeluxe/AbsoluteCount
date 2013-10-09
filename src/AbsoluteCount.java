8d3d370623a7a822dc950aa54c88418e6d789add
Alex K <starmandeluxe2001@gmail.com>
2013-10-09 02:32:26 -0700

    fixed bug

--- src/AbsoluteCount.java
+++ src/AbsoluteCount.java
@@ -5,23 +5,20 @@ public class AbsoluteCount {
 	
 	public static void main(String[] args)
 	{
+		System.out.println(solution(new int[]{-5, -3, -1, 0, 3, 6}));
 		System.out.println(solution(new int[]{-5, -3, -1, -5, -3, -1, 0, 3, 6}));
+		System.out.println(solution(new int[]{2,3,4,2,2,2}));
+		System.out.println(solution(new int[]{2,3,4,-2,-3,2}));
 	}
 	
 	public static int solution(int[] A) {
         int count = A.length;
-        //boolean[] seen = new boolean[A.length];
+
         HashMap<Integer, Boolean> seen = new HashMap<Integer, Boolean>();
         for (int i = 0; i < A.length; i++)
         {
-        	if (A[i] == 0)
-        	{
-        		count--;
-        	}
-        	else
-        	{
         		//if an element has already been encountered, skip it
-        		Integer key = A[i];
+        		Integer key = Math.abs(A[i]);
         		if (seen.get(key) == null)
         		{
         			seen.put(key, new Boolean(true));
@@ -32,7 +29,6 @@ public class AbsoluteCount {
         		}
         		else
         			count--;
-        	}
         }
         return count;
     }
